c4b08d3117652292efee03840a9708747242f334
YanickDL30 <68830387+YanickDL30@users.noreply.github.com>
2020-09-30 18:40:32 -0500

    cup selection using user controlled sprite

--- hello.c
+++ hello.c
@@ -5,7 +5,7 @@
 #include <string.h>
 #define TILE 0xc4
 #define ATTR 0
-#define NUM_ACTORS 3
+#define NUM_ACTORS 4
 
 
 // link the pattern table into CHR ROM
@@ -92,7 +92,7 @@ void display_cups()
 
 void shuffle()
 {
-   char oam_id;
+  char oam_id;
   int i;
   int j;
   setup_graphics();
@@ -145,6 +145,7 @@ void shuffle()
     actor_y[1]=180;
     actor_x[2]=160;
     actor_y[2]=180;
+  
     for(i=0;i<NUM_ACTORS;i++)
     {
      oam_id = oam_meta_spr(actor_x[i], actor_y[i], oam_id, metasprite);
@@ -154,18 +155,44 @@ void guess()
 {
   int num = (rand() % 3) +1;
   int score;
+  int cup=1;  //used to compare selected cup to winning cup
+  char oam_id;
  
   char scr[]="";
   char str[] ="";
   sprintf(str, "%d", num); 
+  
+  actor_x[3]=20;
+  actor_y[3]=80;
+
+  oam_id = oam_meta_spr(actor_x[3], actor_y[3], oam_id, metasprite);
+  
   display_cups();
   
   while(1)
   {
-    if(pad_trigger(0)&PAD_A)
+   
+    if(pad_trigger(0)&PAD_DOWN && (actor_y[3] >=80 && actor_y[3] < 100))
+    {
+      oam_hide_rest(0);
+      cup++;
+      actor_x[3]=20;
+      actor_y[3]+=10;
+      oam_id = oam_meta_spr(actor_x[3], actor_y[3], oam_id, metasprite);
+      
+    }
+    if(pad_trigger(0)&PAD_UP && (actor_y[3] >80 && actor_y[3] <= 100))
+    {
+      oam_hide_rest(0);
+      actor_x[3]=20;
+      actor_y[3]-=10;
+      oam_id = oam_meta_spr(actor_x[3], actor_y[3], oam_id, metasprite);
+    }
+    
+    if(pad_trigger(0)&PAD_START)
     {
       shuffle();
-      if(num==1)
+      if(cup==num)
       {
         ppu_off();
         
@@ -208,125 +235,33 @@ void guess()
 
       break;
      }
-    }
-    
-    if(pad_trigger(0)&PAD_B)
-    {
-      shuffle();
-      if(num==2)
-      {
-        ppu_off();
-        score=score+1;
-        vram_adr(NTADR_A(6,18)); 	
-        vram_write("WIN!", 4);
-
-        vram_adr(NTADR_A(19,19)); 
-        vram_write(str,1);
-        vram_adr(NTADR_A(6,19)); 	
-        vram_write("Winning cup:", 12);
-        
-        
-        sprintf(scr,"%d",score);
-        vram_adr(NTADR_A(13,20)); 
-        vram_write(scr,2);
-        vram_adr(NTADR_A(6,20)); 	
-        vram_write("SCORE:", 6);
-        ppu_on_all();
-        break;
-      }
-     else
-     {
-       ppu_off();
-       vram_adr(NTADR_A(6,18)); 	
-       vram_write("LOSE", 4);
-       
-        vram_adr(NTADR_A(19,19)); 
-        vram_write(str,1);
-        vram_adr(NTADR_A(6,19)); 	
-        vram_write("Winning cup:", 12);
-       
-       
-        sprintf(scr,"%d",score);
-        vram_adr(NTADR_A(13,20)); 
-        vram_write(scr,2);
-        vram_adr(NTADR_A(6,20)); 	
-        vram_write("SCORE:", 6);
-       ppu_on_all();
-       break;
-     }
-    }
-    
-    if(pad_trigger(0)&PAD_SELECT)
-    {
-      shuffle();
-      if(num==3)
-      {
-        ppu_off();
-        score=score+1;
-        vram_adr(NTADR_A(6,18)); 	
-        vram_write("WIN!", 4);
-        
-        vram_adr(NTADR_A(19,19)); 
-        vram_write(str,1);
-        vram_adr(NTADR_A(6,19)); 	
-        vram_write("Winning cup:", 12);
-        
-        
-        sprintf(scr,"%d",score);
-        vram_adr(NTADR_A(13,20)); 
-        vram_write(scr,1);
-        vram_adr(NTADR_A(6,20)); 	
-        vram_write("SCORE:", 6);
-        ppu_on_all();
-        break;
-      }
-      else
-      {
-        ppu_off();
-        vram_adr(NTADR_A(6,18)); 	
-        vram_write("LOSE", 4);
-        
-        vram_adr(NTADR_A(19,19)); 
-        vram_write(str,2);
-        vram_adr(NTADR_A(6,19)); 	
-        vram_write("Winning cup:", 12);
-        
-        
-        sprintf(scr,"%d",score);
-        vram_adr(NTADR_A(13,20)); 
-        vram_write(scr,2);
-        vram_adr(NTADR_A(6,20)); 	
-        vram_write("SCORE:", 6);
-        ppu_on_all();
-
-        break;
-      }
-    }
-  } 
-  
+    }    
+  }  
 }
 
 void game()
 {
 
-  char str[] =""; //score to char string
+  char str[] =""; //score to char string 
   ppu_off();
-
+  
   vram_adr(NTADR_A(8,2));		// set address
   vram_write("CUP GUESSING GAME!", 17);// write bytes to video RAM
  
   vram_adr(NTADR_A(2,6));
-  vram_write("Please choose a cup:",20);
+  vram_write("Please choose a cup using \x1c\x1d",28);
   
-  vram_adr(NTADR_A(2,10));
-  vram_write("\x1f Cup 1 (a)",11);
+  vram_adr(NTADR_A(2,7));
+  vram_write("then press 'start'",18);
   
-  vram_adr(NTADR_A(2,12));
-  //vram_write("Cup 2 (b)",9);
-  vram_write("\x1f Cup 2 (b)",11);
+  vram_adr(NTADR_A(6,10));
+  vram_write("Cup 1",5);
   
-  vram_adr(NTADR_A(2,14));
-  vram_write("\x1f Cup 3 (select)",16);
+  vram_adr(NTADR_A(6,12));
+  vram_write("Cup 2",5);
+  
+  vram_adr(NTADR_A(6,14));
+  vram_write("Cup 3",5);
   
   vram_adr(NTADR_A(6,22));	
   vram_write("                   ", 20);
@@ -353,4 +288,5 @@ void main(void) {
            
   }   
 }
+
 
